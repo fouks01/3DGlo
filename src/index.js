292323a01f80c17d4409751ebdea0973567274a3
Alex Tikhonov <tau9501@gmail.com>
2022-04-20 00:01:08 +0300

    Added lesson19

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import timer from './modules/timer';
+
+
+setInterval(() => {
+    timer('20 April 2022')
+}, 1000);
+
+// timer('23 April 2022');
---
+++ src/modules/timer.js
@@ -0,0 +1,61 @@
+const timer = (deadline) => {
+    const timerHours = document.getElementById('timer-hours');
+    const timerMinutes = document.getElementById('timer-minutes');
+    const timerSeconds = document.getElementById('timer-seconds');
+
+    const getTimeRemaining = () => {
+        let dateStop = new Date(deadline).getTime();
+        let dateNow = new Date().getTime();
+        let timeRemaining = (dateStop - dateNow) / 1000;
+        let hours = Math.floor(timeRemaining / 60 / 60);
+        let minutes = Math.floor((timeRemaining / 60) % 60);
+        let seconds = Math.floor(timeRemaining % 60);
+
+        return {
+            timeRemaining,
+            hours,
+            minutes,
+            seconds
+        };
+    };
+
+    const updateClock = () => {
+        let getTime = getTimeRemaining();
+
+        if (getTime.timeRemaining > 0) {
+            timerHours.textContent = getTime.hours;
+            timerMinutes.textContent = getTime.minutes;
+            timerSeconds.textContent = getTime.seconds;
+
+        } else {
+            getTime.hours = 0;
+            getTime.minutes = 0;
+            getTime.seconds = 0;
+
+        }
+        if (getTime.hours < 10) {
+            timerHours.textContent = '0' + getTime.hours;
+        }
+        if (getTime.minutes < 10) {
+            timerMinutes.textContent = '0' + getTime.minutes;
+        }
+        if (getTime.seconds < 10) {
+            timerSeconds.textContent = '0' + getTime.seconds;
+        }
+        //     setTimeout(updateClock, 1000);
+        //     // setInterval(updateClock, 1000);
+        console.log('updateClock');
+
+        // }
+        // setInterval(() => {
+        //     timer('23 April 2022')
+        // }, 1000);
+
+    };
+
+    updateClock();
+
+
+};
+
+export default timer;
